jvstein/bitcoin-prometheus-exporter

commit 71d2610649dd50323d7953a47b46603524e43fc3

Reuse Proxy client across multiple calls

bitcoind-monitor.py
@@ -9,6 +9,7 @@ import signal
 import sys
 
 from datetime import datetime
+from functools import lru_cache
 from pathlib import Path
 from typing import Any
 from typing import Dict
@@ -146,6 +147,22 @@ def bitcoin_conf_path() -> Path:
     return Path.home() / ".bitcoin" / "bitcoin.conf"
 
 
+@lru_cache(maxsize=1)
+def rpc_client():
+    bitcoin_conf: Path = bitcoin_conf_path()
+    if bitcoin_conf.exists():
+        logger.info("Using config file: %s", bitcoin_conf)
+        return Proxy(btc_conf_file=bitcoin_conf)
+    else:
+        host = BITCOIN_RPC_HOST
+        if BITCOIN_RPC_USER and BITCOIN_RPC_PASSWORD:
+            host = "%s:%s@%s" % (quote(BITCOIN_RPC_USER), quote(BITCOIN_RPC_PASSWORD), host,)
+        if BITCOIN_RPC_PORT:
+            host = "%s:%s" % (host, BITCOIN_RPC_PORT)
+        service_url = "%s://%s" % (BITCOIN_RPC_SCHEME, host)
+        return Proxy(service_url=service_url)
+
+
 @riprova.retry(
     timeout=TIMEOUT,
     backoff=riprova.ExponentialBackOff(),
@@ -156,19 +173,8 @@ def bitcoinrpc(*args) -> RpcResult:
     if logger.isEnabledFor(logging.DEBUG):
         logger.debug("RPC call: " + " ".join(str(a) for a in args))
 
-    bitcoin_conf = bitcoin_conf_path()
-    if bitcoin_conf.exists():
-        proxy = Proxy(btc_conf_file=bitcoin_conf)
-    else:
-        host = BITCOIN_RPC_HOST
-        if BITCOIN_RPC_USER and BITCOIN_RPC_PASSWORD:
-            host = "%s:%s@%s" % (quote(BITCOIN_RPC_USER), quote(BITCOIN_RPC_PASSWORD), host,)
-        if BITCOIN_RPC_PORT:
-            host = "%s:%s" % (host, BITCOIN_RPC_PORT)
-        service_url = "%s://%s" % (BITCOIN_RPC_SCHEME, host)
-        proxy = Proxy(service_url=service_url)
+    result = rpc_client().call(*args)
 
-    result = proxy.call(*args)
     logger.debug("Result:   %s", result)
     return result
 

commit 812cde4c59382866f236ee6bd4dc71b9d46e5c94

Fix error caused by Proxy object retries
Fix for regression caused by 71d2610649dd50323d7953a47b46603524e43fc3

There were many unexpected errors caused by retries on the Proxy object.
This will create a new object for each request, while still only looking
for the config file once.

bitcoind-monitor.py
@@ -148,11 +148,11 @@ def bitcoin_conf_path() -> Path:
 
 
 @lru_cache(maxsize=1)
-def rpc_client():
+def rpc_client_factory():
     bitcoin_conf: Path = bitcoin_conf_path()
     if bitcoin_conf.exists():
         logger.info("Using config file: %s", bitcoin_conf)
-        return Proxy(btc_conf_file=bitcoin_conf)
+        return lambda: Proxy(btc_conf_file=bitcoin_conf)
     else:
         host = BITCOIN_RPC_HOST
         if BITCOIN_RPC_USER and BITCOIN_RPC_PASSWORD:
@@ -160,7 +160,11 @@ def rpc_client():
         if BITCOIN_RPC_PORT:
             host = "%s:%s" % (host, BITCOIN_RPC_PORT)
         service_url = "%s://%s" % (BITCOIN_RPC_SCHEME, host)
-        return Proxy(service_url=service_url)
+        return lambda: Proxy(service_url=service_url)
+
+
+def rpc_client():
+    return rpc_client_factory()()
 
 
 @riprova.retry(